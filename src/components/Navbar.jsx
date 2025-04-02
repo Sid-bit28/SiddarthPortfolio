06fdfa244a133e1926afcf1e2e9b27c9b8e6fc93
Siddarth Banerjee <siddarth2801@gmail.com>
2025-04-03 02:46:44 +0530

    Navbar completed

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-scroll';
+
+import { MENU_LINKS } from '../utils/data';
+import LOGO from '../assets/images/favicon.ico';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setIsOpen(true); // Always show menu on large screen.
+      } else {
+        setIsOpen(false); // Hide menu by default on small screens.
+      }
+    };
+
+    // Set initial state based on screen size
+    handleResize();
+
+    // Listen to resize events
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+  return (
+    <nav className="container mx-auto sticky top-5 z-10">
+      <div className="flex items-center justify-between rounded-full bg-white/25 border-[#fee6cc] backdrop-blur-[10px] m-5 p-3 md:p-0">
+        {/* Logo */}
+        <img className="h-13 ml-1 -mb-1" src={LOGO} alt="Logo" />
+
+        {/* Hamburger Menu (Visible only on small screen) */}
+        <button
+          className="block md:hidden text-[#333] mr-6 focus:outline-none"
+          onClick={toggleMenu}
+        >
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            {isOpen ? (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            ) : (
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              />
+            )}
+          </svg>
+        </button>
+
+        {/* Navigation Links */}
+        <ul className={`${isOpen ? 'flex' : 'hidden'} menu-wrapper`}>
+          {MENU_LINKS.map(item => (
+            <li key={item.id}>
+              <Link
+                activeClass="active"
+                to={item.to}
+                smooth
+                spy
+                offet={item.offset}
+                className="menu-item"
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        {/* Hire Me Button */}
+        <button className="hidden md:block h-12 text-[15px] font-medium text-white bg-purple-800 rounded-full px-9 transition-transform duration-300 ease-in-out hover:scale-105">
+          Hire Me
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/utils/data.js
@@ -0,0 +1,164 @@
+import {
+  FaReact,
+  FaHtml5,
+  FaCss3,
+  FaNodeJs,
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+} from 'react-icons/fa';
+import { BiLogoJavascript } from 'react-icons/bi';
+import { SiExpress, SiMongodb, SiMysql, SiWebpack } from 'react-icons/si';
+import {
+  VscVscode,
+  VscCommentUnresolved,
+  VscTerminalPowershell,
+} from 'react-icons/vsc';
+import { FaGitAlt, FaXTwitter, FaDribbble } from 'react-icons/fa6';
+import { MdGroups3 } from 'react-icons/md';
+
+import PROJECT_IMG_1 from '../assets/images/project-1.png';
+import PROJECT_IMG_2 from '../assets/images/project-2.png';
+import PROJECT_IMG_3 from '../assets/images/project-3.png';
+import PROJECT_IMG_4 from '../assets/images/project-4.png';
+import PROJECT_IMG_5 from '../assets/images/project-5.png';
+import PROJECT_IMG_6 from '../assets/images/project-6.png';
+
+export const MENU_LINKS = [
+  { id: '01', label: 'Home', offset: -100, to: 'hero' },
+  { id: '02', label: 'Skills', offset: -80, to: 'skills' },
+  { id: '03', label: 'About Me', offset: -80, to: 'about' },
+  { id: '04', label: 'Projects', offset: -80, to: 'projects' },
+  { id: '05', label: 'Contact', offset: -80, to: 'contact' },
+];
+
+export const STATS = [
+  { id: '01', count: '10', label: 'Years of \nExperience' },
+  { id: '02', count: '12', label: 'Certifications \nEarned' },
+  { id: '03', count: '43', label: 'Projects \nCompleted' },
+  { id: '04', count: '17+', label: 'Happy \nClients' },
+];
+
+export const SKILLS_TABS = [
+  { id: '01', label: 'All', value: 'all' },
+  { id: '02', label: 'Frontend', value: 'frontend' },
+  { id: '03', label: 'Backend', value: 'backend' },
+  { id: '04', label: 'Tools', value: 'tools' },
+  { id: '05', label: 'Skills', value: 'soft-skills' },
+];
+
+export const SKILLS = [
+  {
+    id: '01',
+    icon: FaReact,
+    skill: 'React JS',
+    progress: 95,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+  {
+    id: '02',
+    icon: FaHtml5,
+    skill: 'HTML',
+    progress: 99,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+  {
+    id: '03',
+    icon: FaCss3,
+    skill: 'CSS',
+    progress: 65,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+  {
+    id: '04',
+    icon: FaReact,
+    skill: 'React JS',
+    progress: 95,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+  {
+    id: '01',
+    icon: FaReact,
+    skill: 'React JS',
+    progress: 95,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+  {
+    id: '01',
+    icon: FaReact,
+    skill: 'React JS',
+    progress: 95,
+    type: 'frontend',
+    description:
+      'Experienced in building scalable, components-driven web applications using React.js.',
+  },
+];
+
+export const ABOUT_ME = {
+  content: `I'm a passionate developer with a strong foundation in building responsive, user-friendly websites.`,
+  socialLinks: [
+    { id: '01', label: 'Facebook', icon: FaFacebook, link: '#' },
+    { id: '02', label: 'Instagram', icon: FaInstagram, link: '#' },
+    { id: '03', label: 'Twitter', icon: FaXTwitter, link: '#' },
+    { id: '04', label: 'LinkedIn', icon: FaLinkedin, link: '#' },
+  ],
+
+  email: 'siddarth2801@gmail.com',
+  phone: '+91-7209582296',
+  website: 'yupppp',
+};
+
+export const PROJECTS = [
+  {
+    id: 1,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+  {
+    id: 2,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+  {
+    id: 3,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+  {
+    id: 4,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+  {
+    id: 5,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+  {
+    id: 6,
+    title:
+      'Full Stack Notes App using MERN | MongoDB, Express, React JS, Node JS',
+    image: PROJECT_IMG_1,
+    tags: ['React, Express.js', 'MongoDB', 'Node.js'],
+  },
+];
